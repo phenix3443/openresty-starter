69e40cd91195f8dc6949e81e22386967fb93112e
liushangliang <phenix3443@gmail.com>
2018-07-10 20:10:36 +0800

    添加脚本封装的部分代码

---
+++ script/config.py
@@ -0,0 +1,41 @@
+# python3
+# -*- coding:utf-8 -*-
+# author:liushangliang@xunlei.com
+
+import os
+import sys
+import logging
+
+script_path = os.path.split(os.path.realpath(__file__))[0]
+project_path = os.path.normpath("{}/..".format(script_path))
+
+sys.path.insert(0, os.path.join(project_path, "scrip/"))
+
+sub_loggers = []
+
+
+def add_logger(l):
+    sub_loggers.append(l)
+
+
+sub_modules = [
+    "main_db",
+    "task_db",
+    "resource_db",
+    "collect_db",
+    "database",
+    "xllog",
+    "block",
+]
+
+for m in sub_modules:
+    l = logging.getLogger(m)
+    add_logger(l)
+
+main_db = {
+    "host": "",
+    "port": 3306,
+    "user": "root",
+    "password": "",
+    "database": "test",
+}
---
+++ script/main_db.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author:liushangliang@xunlei.com
+"""mysql数据库接口"""
+import logging
+import mysql.connector
+
+import config
+import xllog
+
+logger = logging.getLogger(__name__)
+
+
+class MainDB():
+    def __init__(self, cfg):
+        self.conn = mysql.connector.connect(
+            pool_name="main_db_poll", pool_size=32, **cfg)
+        self.conn.autocommit = True
+
+    def get_conn(self):
+        return self.conn
+
+    def __del__(self):
+        self.conn.close()
+
+    def get_version(self):
+        """"""
+        cursor = self.conn.cursor(dictionary=True)
+        stmt = "select version();"
+        logger.debug(stmt)
+        cursor.execute(stmt)
+        row = cursor.fetchone()
+        logger.debug(row)
+        return row
+
+
+if __name__ == "__main__":
+    config.add_logger(logger)
+    xllog.init_logger(config.sub_loggers)
+
+    mdb = MainDB(config.main_db)
+    cursor = mdb.get_conn().cursor()
+    res = mdb.get_version()
+    print(res)
