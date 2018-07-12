e5b5789ede175afd0cc72ba3bbec77ca6575070c
liushangliang <liushangliang@xunlei.com>
2018-07-13 00:29:03 +0800

    添加接口模板

---
+++ src/lua/interface.lua
@@ -0,0 +1,9 @@
+-- -*- coding:utf-8 -*-
+-- author:liushangliang@xunlei.com
+-- desc:对外接口代码示例
+
+local cjson = require("cjson.safe")
+local main_db = require("database.main_db")
+
+local function main()
+end
