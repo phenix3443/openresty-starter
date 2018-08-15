78b25d58d827293bac33701bff8fbe3c4218d1fe
liushangliang <phenix3443@gmail.com>
2018-08-15 21:24:54 +0800

    优化代码

--- src/lua/database/main_db.lua
+++ src/lua/database/main_db.lua
@@ -5,10 +5,10 @@
 local cjson = require("cjson.safe")
 local mysql = require("resty.mysql")
 
-local MainDB = {}
-local mt = {__index = MainDB}
+local export = {}
+local mt = {__index = export}
 
-function MainDB.new(db_cfg)
+function export.new(db_cfg)
     local db, err = mysql:new()
     if not db then
         ngx.log(ngx.ERR,"failed to instantiate mysql: ", err)
@@ -27,7 +27,7 @@ function MainDB.new(db_cfg)
     return setmetatable({db=db},mt)
 end
 
-function MainDB.close(self)
+function export.close(self)
     local ok, err = self.db:set_keepalive(10000, 100)
     if not ok then
         ngx.log(ngx.ERR, "failed to set keepalive: ", err)
@@ -36,7 +36,7 @@ function MainDB.close(self)
 end
 
 -- 下面自行编写业务代码
-function MainDB.get_db_version(self)
+function export.get_db_version(self)
     -- 查询用户下载信息
     local stmt = string.format("select version() as version;")
     ngx.log(ngx.DEBUG, stmt)
@@ -50,4 +50,30 @@ function MainDB.get_db_version(self)
     return resp
 end
 
-return MainDB
+function export.get_info(self, cond)
+    local values = {}
+    for i,v in pairs(cond) do
+        if tonumber(v) then
+            table.insert(values, string.format("%s=%d",i,tonumber(v)))
+        else
+            table.insert(values, string.format("%s=%s",i, ngx.quote_sql_str(v)))
+        end
+    end
+
+    if #values < 1 then
+        return
+    end
+
+    local stmt = string.format("select * from tb_name where %s;", table.concat(values," and "))
+    ngx.log(ngx.DEBUG, stmt)
+    local res, err, errcode, sqlstate = self.db:query(stmt)
+    if not res then
+        ngx.log(ngx.ERR,string.format("%s:%s",errcode, err))
+        return
+    end
+    local resp = res
+    ngx.log(ngx.DEBUG, "resp:", resp)
+    return resp
+end
+
+return export
