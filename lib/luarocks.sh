98e85209c3af35e9b4b3cfb26707b52d975130ef
phenix3r3 <phenix3443@gmail.com>
2018-08-24 00:17:22 +0800

    增加luaunit做测试

--- lib/luarocks.sh
+++ lib/luarocks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # desc：安装一些依赖的第三方库
-LIBS="lua-cjson penlight version lua-resty-http"
+LIBS="lua-cjson penlight version lua-resty-http luaunit"
 for lib in ${LIBS}; do
     echo ${lib}
     luarocks install ${lib}
