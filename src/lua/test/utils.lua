a31a31431fc86dc7c4a7da550a22418290b18e34
phenix3r3 <phenix3443@gmail.com>
2018-08-24 00:18:30 +0800

    增加测试用例

--- src/lua/test/utils.lua
+++ src/lua/test/utils.lua
@@ -3,23 +3,23 @@
 -- desc: 测试utils/utils.lua
 -- doc:
 local cjson = require("cjson.safe")
+local lu = require("luaunit")
 local utils = require("utils.utils")
 
-local function test_get_favor_lang()
-    local hds = ngx.req.get_headers()
-    local accept_lang = hds["Accept-language"]
+function test_get_favor_lang()
+    local favor = utils.get_favor_lang("fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5")
+    lu.assertEquals(favor,"fr-CH")
 
-    if not accept_lang then
-        ngx.log(ngx.DEBUG,"not accept language header")
-        return
-    end
+    favor = utils.get_favor_lang("zh")
+    lu.assertEquals(favor,"zh")
 
-    local favor = utils.get_favor_lang(accept_lang)
-
+    favor = utils.get_favor_lang("")
+    lu.assertEvalToFalse(favor, nil)
 end
 
+
 local function main()
-    test_get_favor_lang()
+    lu.LuaUnit.run()
 end
 
 main()
