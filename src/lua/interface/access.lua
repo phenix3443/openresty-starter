b27e5d1b1612ce971683ffe15b4b427b6720e57b
liushangliang <phenix3443@gmail.com>
2018-11-01 21:00:28 +0800

    使用resty风格返回错误

--- src/lua/interface/access.lua
+++ src/lua/interface/access.lua
@@ -6,71 +6,69 @@ local cjson = require("cjson.safe")
 
 local utils = require("utils.utils")
 
-local function check_method()
-    return true
-end
+
 
 -- 检查url中的query参数
 local function check_query()
     local args = ngx.req.get_uri_args()
-    ngx.log(ngx.DEBUG,"query args:", cjson.encode(args))
-
-    local required = {}  -- 修改, url必备参数
+    local required = {}
     for _, k in ipairs(required) do
         if not args[k] then
-            ngx.log(ngx.ERR, "url lack ", k)
-            return
+            local err_msg = string.format("url 缺少参数:%s",k)
+            utils.send_resp(ngx.HTTP_BAD_REQUEST, err_msg)
         end
     end
-
-    return true
 end
 
 local function check_header()
     local hd = ngx.req.get_headers()
-    ngx.log(ngx.DEBUG, "headers:", cjson.encode(hd))
-
-    local required = {}  -- 修改, header必备参数
+    local required = {"cookie"}
     for _, k in ipairs(required) do
         if not hd[k] then
-            ngx.log(ngx.ERR, "header lack ", k)
-            return
+            local err_msg = string.format("header 缺少参数:%s",k)
+            utils.send_resp(ngx.HTTP_BAD_REQUEST, err_msg)
         end
     end
-    return true
 end
 
 local function check_cookie()
-    local required = {}  -- 修改, header必备参数
+    local required = {}
     for _, k in ipairs(required) do
         if not ngx.var["cookie_"..k] then
-            ngx.log(ngx.ERR, "cookie lack:", k)
-            return
+            local err_msg = string.format("cookie 缺少参数:%s",k)
+            utils.send_resp(ngx.HTTP_BAD_REQUEST, err_msg)
         end
     end
-
-    return true
 end
 
 local function check_body()
     ngx.req.read_body()
-    local body = ngx.req.get_post_args()
+    local body = ngx.req.get_body_data()
+    if not body then
+        local err_msg = string.format("缺少body")
+        utils.send_resp(ngx.HTTP_BAD_REQUEST, err_msg)
+    end
 
-    ngx.log(ngx.DEBUG,"body:", cjson.encode(body))
+    local decode_body = cjson.decode(body)
+    if not decode_body then
+        local err_msg = string.format("json decode body failed")
+        utils.send_resp(ngx.HTTP_BAD_REQUEST, err_msg)
+    end
 
-    return true
+    local  required = {}
+    for _,k in pairs(required) do
+        if decode_body[k] then
+            local err_msg = string.format("body 缺少参数:%s", k)
+            utils.send_resp(ngx.HTTP_BAD_REQUEST, err_msg)
+        end
+    end
 end
 
 local function main()
-    if check_query() and
-        check_header() and
-        check_cookie() and
-        check_body()
-    then
-        ngx.log(ngx.DEBUG, "check common req params pass")
-    else
-        utils.send_resp("ERR_PARAM")
-    end
+    check_query()
+    check_header()
+    check_cookie()
+    check_body()
 end
 
 main()
