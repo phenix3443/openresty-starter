c463af16aed5b96a526da1f58ec813edbddd55fd
liushangliang <liushangliang@xunlei.com>
2018-12-14 21:20:07 +0800

    增加falcon上报类

--- script/falcon.py
+++ script/falcon.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 # author:phenix3443+github@gmail.com
-"""mysql数据库接口"""
+"""falcon上报接口"""
 
-import os
 import json
+import socket
+import time
+import logging
 import requests
 
-agent = {
-    "host": "127.0.0.1",
-    "port": 1988,
-}
+HOSTNAME = socket.gethostname()
+
+logger = logging.getLogger(__name__)
 
 
-def get_hostname():
-    res_command = os.popen('hostname').read().strip()
-    return res_command if res_command else 'unknown'
+class Falcon():
+    def __init__(self, cfg):
+        self.url = "{}/v1/push".format(cfg["uri"])
+
+    def report_example(self, count):
+        payloads = [{
+            "endpoint": HOSTNAME,
+            "metric": "example_metric",
+            "timestamp": int(time.time()),
+            "step": 60,
+            "value": count,
+            "counterType": "GAUGE",
+            "tags": "project=example-project"
+        }]
+        logger.debug(payloads)
+        r = requests.post(self.url, data=json.dumps(payloads))
+        if r.status_code != 200:
+            logger.debug(r.text)
 
 
-def report(payloads):
-    agent_url = "http://{}:{}/v1/push".format(agent["host"], agent["port"])
-    r = requests.post(agent_url, data=json.dumps(payloads))
-    print(r.text)
+if __name__ == '__main__':
+    pass
