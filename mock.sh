4e0e2372e1d9f88e19438d52c846b522f5c7528d
phenix3443 <phenix3443@gmail.com>
2019-01-30 16:48:01 +0800

    完善 mock 环境脚本

--- mock.sh
+++ mock.sh
@@ -7,27 +7,35 @@ NGINX_DIR=${PROJECT_DIR}/nginx
 nginx_port=1235
 echo ${SCRIPT_DIR}
 
-DB_CID=$(docker run -d \
-                --name example-mysql \
+DB_CID=$(docker run \
+                -d \
                 --restart always \
                 -e MYSQL_ROOT_PASSWORD="" \
                 mysql:5.7)
 
-CACHE_CID=$(docker run -d \
+CACHE_CID=$(docker run \
+                   -d \
                    --restart always \
-                   --name example-redis \
                    redis:4)
 
-MOCK_CID=$(docker run -d \
-                  --name example \
+MOCK_CID=$(docker run \
+                  -d \
                   --restart always \
-                  --link example-mysql:mysql \
-                  --link example-redis:redis \
+                  --name example \
+                  --link ${DB_CID}:mysql \
+                  --link ${CACHE_CID}:redis \
                   -v ${NGINX_DIR}/conf/project.proxy.nginx.conf:/usr/local/openresty/nginx/conf/project.proxy.nginx.conf \
                   -v ${NGINX_DIR}/conf/nginx.conf:/usr/local/openresty/nginx/conf/nginx.conf \
                   -v ${NGINX_DIR}/conf/vhosts:/usr/local/openresty/nginx/conf/vhosts \
                   -v ${NGINX_DIR}/lua:/usr/local/openresty/nginx/lua \
                   -v ${NGINX_DIR}/lib:/usr/local/openresty/nginx/lib \
-           openresty/openresty:1.13.6.2-2-centos)
+                  -v ${NGINX_DIR}/logs:/usr/local/openresty/nginx/logs \
+                  -p 9000:80 \
+                  openresty/openresty:1.13.6.2-2-centos)
 
-# echo "mock_cid: "${MOCK_CID}
+
+# 使用 luarocks 安装依赖
+docker exec example yum install -y gcc
+docker exec example sh /usr/local/openresty/nginx/lib/luarocks.sh
+
+echo "example mock cid: "${MOCK_CID}
