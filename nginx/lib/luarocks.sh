f933f84a705a7fa75f093398640041e9e0a72742
phenix3443 <phenix3443@gmail.com>
2019-03-01 19:46:47 +0800

    修改 luarocks 文件安装路径

--- nginx/lib/luarocks.sh
+++ nginx/lib/luarocks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # desc：安装一些依赖的第三方库
 LIBS="lua-cjson penlight version lua-resty-http luaunit ldoc lua-discount"
-INSTALL_DIR=<example-dir>
+INSTALL_DIR=example-dir
 for lib in ${LIBS}; do
     echo ${lib}
     luarocks --tree ${INSTALL_IDR} install ${lib}
