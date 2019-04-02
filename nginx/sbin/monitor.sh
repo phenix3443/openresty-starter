3d419758c39669672e2526da37fb025fcd3a1449
phenix3443 <phenix3443@gmail.com>
2019-04-02 14:49:48 +0800

    优化代码

--- nginx/sbin/monitor.sh
+++ nginx/sbin/monitor.sh
@@ -5,14 +5,14 @@
 # Settings nginx
 SERVER_NAME="example-bin"
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
-NGINX_DIR=$(cd ${SCRIPT_DIR}/../;pwd)
-DT=$(date +'%Y-%m-%dT%H:%M:%S')
+PROJECT_DIR=$(cd ${SCRIPT_DIR}/../;pwd)
+DT=$(date +'%F %R:%S')
 
 num=$(ps -ef | grep ${SERVER_NAME} | grep -vE 'grep|monitor' | wc -l)
-echo ${num}
+
 if [ ${num} -lt 1 ];then
-    echo "${DT}, restart nginx now" >> ${NGINX_DIR}/logs/monitor.log
-    ${NGINX_DIR}/sbin/${SERVER_NAME}
+    echo "${DT}, restarting" >> ${PROJECT_DIR}/logs/monitor.log
+    ${PROJECT_DIR}/sbin/${SERVER_NAME}
 else
-    echo "${DT}, run normal" >> ${NGINX_DIR}/logs/monitor.log
+    echo "${DT}, running" >> ${PROJECT_DIR}/logs/monitor.log
 fi
