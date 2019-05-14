61e20e6ebca244eba38259c90fd3145c6bba4855
phenix3443 <phenix3443@gmail.com>
2019-05-14 22:12:29 +0800

    使用绝对路径安装

--- nginx/sbin/luarocks.sh
+++ nginx/sbin/luarocks.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # desc：安装一些依赖的第三方库
-LIBS="lua-cjson penlight version lua-resty-http luaunit ldoc lua-discount serpent luacov cluacov"
-INSTALL_DIR=../luarocks
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+PROJECT_DIR=$(cd ${SCRIPT_DIR}/../;pwd)
+INSTALL_DIR=${PROJECT_DIR}/luarocks
+
+LIBS="lua-cjson penlight version lua-resty-http luaunit ldoc lua-discount serpent luacov cluacov mmdblua lua-resty-jit-uuid"
+
 for lib in ${LIBS}; do
     echo ${lib}
     luarocks --tree=${INSTALL_DIR} install ${lib}
