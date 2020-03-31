2c51d3280638a534d3dff427d161c5f55053a0f0
phenix3443 <phenix3443@gmail.com>
2020-03-31 17:57:25 +0800

    优化代码

--- nginx/lua/cache/cache.lua
+++ nginx/lua/cache/cache.lua
@@ -15,17 +15,13 @@ local M = {}
 --- 示例程序
 -- 获取 example_cache 实例的相关信息
 function M.get_example_cache_info()
-    local example_cache = ExampleCache(ucfg.example_cache)
-    if not example_cache:is_connected() then
-        return
-    end
-    local info = example_cache:get_info()
-    example_cache:close()
+    local c = ExampleCache(ucfg.example_cache)
+    local info = c:get_info()
+    c:close()
 
     return info
 end
 
 -- 以下是具体的接口 -----------------------------------------------------------
-
 
 return M
