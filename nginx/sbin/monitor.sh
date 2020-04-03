679b3f0857f68cfbc239e370f6319a6694f9823d
phenix3443 <phenix3443@gmail.com>
2020-04-03 20:36:31 +0800

    优化脚本

--- nginx/sbin/monitor.sh
+++ nginx/sbin/monitor.sh
@@ -3,12 +3,14 @@
 # author:phenix3443+github@gmail.com
 # description: nginx http server.
 # Settings nginx
-SERVER_NAME="example-bin"
+
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 PROJECT_DIR=$(cd ${SCRIPT_DIR}/../;pwd)
 DT=$(date +'%F %R:%S')
 
-num=$(ps -ef | grep ${SERVER_NAME} | grep -vE 'grep|monitor' | wc -l)
+
+pid=$(cat ${PROJECT_DIR}/logs/nginx.pid )
+num=$(ps -ef | grep ${pid} | grep -vE 'grep|monitor' | wc -l)
 
 if [ ${num} -lt 1 ];then
     echo "${DT}, restarting" >> ${PROJECT_DIR}/logs/monitor.log
