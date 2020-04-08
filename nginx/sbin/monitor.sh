06ff2b3efe4417e03193fd87ee7830b8d6130220
phenix3443 <phenix3443@gmail.com>
2020-04-08 10:36:20 +0800

    修正监控脚本

--- nginx/sbin/monitor.sh
+++ nginx/sbin/monitor.sh
@@ -7,14 +7,12 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 PROJECT_DIR=$(cd ${SCRIPT_DIR}/../;pwd)
 DT=$(date +'%F %R:%S')
+PROJECT=<custom>
 
-
-pid=$(cat ${PROJECT_DIR}/logs/nginx.pid )
-num=$(ps -ef | grep ${pid} | grep -vE 'grep|monitor' | wc -l)
-
+num=$(ps -ef | grep ${PROJECT} | grep -vE 'grep|monitor' | wc -l)
 if [ ${num} -lt 1 ];then
-    echo "${DT}, restarting" >> ${PROJECT_DIR}/logs/monitor.log
-    ${PROJECT_DIR}/sbin/${SERVER_NAME}
+    echo "${DT}, ${PROJECT} restarting" >> ${PROJECT_DIR}/logs/monitor.log
+    ${PROJECT_DIR}/sbin/nginx
 else
-    echo "${DT}, running" >> ${PROJECT_DIR}/logs/monitor.log
+    echo "${DT}, ${PROJECT} running" >> ${PROJECT_DIR}/logs/monitor.log
 fi
