19c1452aa68b129b10ff97be35093275fa53c48c
phenix3443 <phenix3443@gmail.com>
2020-04-26 10:35:04 +0800

    优化代码

--- nginx/lua/database/database.lua
+++ nginx/lua/database/database.lua
@@ -11,20 +11,15 @@ local ExampleDB = require("database.example")
 
 local M = {}
 
-
 --- 示例程序
 -- 获取 example_db 版本信息
-function M.get_example_db_version()
-    local example_db = ExampleDB(ucfg.example_db)
-    if not example_db:is_connected() then
-        return
-    end
-    local version = example_db:get_mysql_version()
-
-    example_db:close()
-
+function M.get_db_version()
+    local db = ExampleDB(ucfg.db)
+    local version = db:get_mysql_version()
+    db:close()
     ngx.log(ngx.DEBUG, "mysql version:", version)
 end
+
 
 -- 下面自行编写业务代码 -------------------------------------------------------
 
