6d27d5daa6760c84dfcfb6fe2dd69e8c95c3b3b5
phenix3443 <phenix3443@gmail.com>
2020-05-08 20:11:52 +0800

    使用雪花算法生成唯一id

--- nginx/sbin/luarocks.sh
+++ nginx/sbin/luarocks.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd $(dirname $0);pwd)
 PROJECT_DIR=$(cd ${SCRIPT_DIR}/../;pwd)
 INSTALL_DIR=${PROJECT_DIR}/luarocks
 
-LIBS="lua-cjson penlight version lua-resty-http luaunit ldoc lua-discount serpent luacov cluacov mmdblua lua-resty-jit-uuid"
+LIBS="lua-cjson penlight version lua-resty-http luaunit ldoc lua-discount serpent luacov cluacov mmdblua snowflake"
 
 for lib in ${LIBS}; do
     echo ${lib}
